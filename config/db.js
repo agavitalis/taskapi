6b9151a49348961072a7b4d7201b1c4d2e2fcda3
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2019-11-10 16:45:22 +0100

    database connections and readme configured

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose')
 
 //connect to a local mongo DB
-mongoose.connect("mongodb://localhost:27017/task-api", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect("mongodb://localhost:27017/taskapi-v", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+//connect to an online mongo DB
+mongoose.connect("mongodb://localhost:27017/taskapi-v", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 //check if the connection was successful
 mongoose.connection.on('error', console.error.bind(console, 'connection error'))
