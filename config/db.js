64247a0406e372fdae5f91a74ac67ffc7ee9320c
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2019-11-10 16:54:29 +0100

    database connections and readme configured

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose')
 
 //connect to a local mongo DB
-mongoose.connect("mongodb://localhost:27017/taskapi-v", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+//mongoose.connect("mongodb://localhost:27017/taskapi-v", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
-//connect to an online mongo DB
-mongoose.connect("mongodb://localhost:27017/taskapi-v", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+//before going live, uncomment this connection
+mongoose.connect('mongodb://vitalis:vitalis123@ds263307.mlab.com:63307/taskapi-v', {
+    useNewUrlParser: true
+});
 
 //check if the connection was successful
 mongoose.connection.on('error', console.error.bind(console, 'connection error'))
